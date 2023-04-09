13e74755c8b8098eaa9f71056862536b4937ec1e
Anastasiia <anstasiakim2789@gmail.com>
2023-04-09 19:41:24 +0200

    add drag and drop to upload images in /users/publication

--- src/Publication.js/CreatePublication.js
+++ src/Publication.js/CreatePublication.js
@@ -1,44 +1,45 @@
 
 import React,{useState, useEffect,useRef} from "react"
-import { Box, Flex, Text, Button, Stack, Img, HStack,Avatar,Hide,Show ,Input,InputGroup,InputLeftElement,Alert,
-    Image,
-    AlertTitle,
-    AlertDescription,
- } from "@chakra-ui/react";
+import { Box, Flex, Text, Button ,Input,InputGroup,InputLeftElement,Alert,Image} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import Commons from "../Utility/Commons";
-import {AppstoreAddOutlined} from '@ant-design/icons';
+
 import { useCookies } from 'react-cookie'; 
 import { useNavigate   } from "react-router-dom";
-import FormatDate from "../Utility/FormatDate";
+
+import { Upload, Form } from 'antd';
+
+
 
 export default function CreatePublication(props){
 
-    const [selectedFile, setSelectedFile] = useState(null);
+
     const [comment, setComment]=useState("")
-    const img = useRef(null)
     const [cookieObjectApiKey, setCookieObjectApiKey, removeCookiObjectApiKey] = useCookies(['apiKey', "id", "email"]);
-    
+    const [myFile, setMyFile]=useState()
+
+
     let publishPublication=async()=>{
         const formData = new FormData();
-        formData.append('myImage', selectedFile);
+        formData.append('myImage', myFile);
         formData.append('comment', comment);
-        if(selectedFile!==null){
+        if(myFile!==null){
             let response = await fetch (Commons.baseUrl+"/mediaPost?apiKey="+cookieObjectApiKey.apiKey,{
                 method: 'POST',
                 body:formData
             })
         }
-        img.current.value=""
+
         setComment("")
         console.log(cookieObjectApiKey.apiKey)
         console.log(cookieObjectApiKey.id)
+
     }
 
-
-    const onChangeFile = (event) => {
-        setSelectedFile(event.target.files[0]);
-    };
+    let chageValueImage =(file)=>{
+        setMyFile(file)
+        console.log("Test")
+    }
 
     return(
         <Box  pt="100px" minH={["90vh"]} alignItems={"center"} justifyContent={"center"} display={"flex"} flexDirection={"column"} >
@@ -48,20 +49,19 @@ export default function CreatePublication(props){
                     <Text fontSize={"xl"} textAlign={"center"} >Make your new post</Text>
                 </Box>
                 <Box flexDirection={"column"}  minH={["60vh"]}  display={"flex"} justifyContent={"center"} alignItems={"center"} >
-                    <img  src="/images/photo-svgrepo-com.svg" ></img>
-                    <Text fontSize={"xl"} >Drag photos and videos here</Text>
-                    <Input
-                        errorBorderColor='crimson'
-                        name="myImage" 
-                        type="file"
-                        placeholder="Select from computer"
-                        accept=".png" 
-                        mt={"2%"}
-                        mb={"1%"}
-                        onChange={onChangeFile}
-                        ref={img}
-                        w={["40%"]}
-                    />
+                    
+                    <Box  w={"100%"} display={"flex"} justifyContent={"center"}>
+                        <Form.Item name="image">
+                            <Upload  action={ (file) => {chageValueImage(file)} }  listType="picture">
+                                <Text  style={{fontSize: '20px', width:"400px", borderWidth:"1px", borderStyle:"dashed solid", padding:"10px", textAlign:"center" }} >
+                                    <Box display={"flex"} justifyContent={"center"}>
+                                        <img  src="/images/photo-svgrepo-com.svg" ></img>
+                                    </Box>
+                                    <Text mb={"20px"}>Click or drag file to this area to upload </Text>
+                                </Text>
+                            </Upload>
+                        </Form.Item>
+                    </Box>
                     <Input
                         placeholder="Add a comment to your post"
                         w={["40%"]}
@@ -69,6 +69,7 @@ export default function CreatePublication(props){
                         onChange={(e)=>setComment(e.target.value)}
                         value={comment}
                     />
+               
                     <Flex mt={"4%"} w={"100%"} justifyContent={"center"}  >
                         <Button color={"white"} w={"50%"} bg="#0077FF" onClick={publishPublication} >Post</Button>
                     </Flex>
