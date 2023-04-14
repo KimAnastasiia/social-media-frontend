26983b2ab788d51d06c05350c5b68ff9639085cf
Anastasiia <anstasiakim2789@gmail.com>
2023-04-14 18:38:39 +0200

    made it possible to like comments

--- src/Publication.js/CommentsPublication.js
+++ src/Publication.js/CommentsPublication.js
@@ -25,12 +25,13 @@ import {
     PopoverAnchor,Portal,useDisclosure
   } from '@chakra-ui/react'
   import Icon, { HomeOutlined } from '@ant-design/icons';
+
 export default function CommentsPublication (props){
 
     const [cookieObjectApiKey, setObjectApiKey, removeCookiObjectApiKey] = useCookies(['apiKey', "id", "email"]);
     const [modifiedComment, setModifiedComment]=useState("")
     let selectedCommentId = useRef(-1)
-    let [listOfCountLikes, setListOfCountLikes]=useState([])
+    const [listOfCountLikes, setListOfCountLikes]=useState([])
 
     useEffect (()=>{ 
         props.getComments()
@@ -80,6 +81,24 @@ export default function CommentsPublication (props){
           <path  stroke="black" stroke-width="40"  d="M923 283.6c-13.4-31.1-32.6-58.9-56.9-82.8-24.3-23.8-52.5-42.4-84-55.5-32.5-13.5-66.9-20.3-102.4-20.3-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5-24.4 23.9-43.5 51.7-56.9 82.8-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3 0.1-35.3-7-69.6-20.9-101.9z" />
         </svg>
       );
+
+    let addLike=async(commentId)=>{
+        let response = await fetch (Commons.baseUrl+"/likes?apiKey="+cookieObjectApiKey.apiKey,{
+
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            body:
+                JSON.stringify({ 
+                    postId:props.postId,
+                    commentId:commentId
+                })
+        })
+        props.getComments()
+        coutLikes()
+    }
 return(
     <Box>
         {props.commentsUsers.sort((a,b)=>b.date-a.date)
@@ -135,9 +154,11 @@ return(
                                     return <Text>{d.totalLikes}</Text>
                                 }
                             })}
-                            <Icon component={HeartSvg} style={{
-                                    fontSize: "20px",
-                                }} />
+                            <Box onClick={()=>{addLike(commentUser.id)}} >
+                                <Icon component={HeartSvg} style={{
+                                        fontSize: "20px",
+                                    }} />
+                            </Box>
                         </Box>
                     </Box>
                 </Box>
