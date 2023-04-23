2ef7339cdd3223e7c71be197886780d3a6863f00
Anastasiia <anstasiakim2789@gmail.com>
2023-04-23 19:39:27 +0200

    navigation to the profile or to the login page by clicking on the penguin

--- src/CommonParts/Menu.js
+++ src/CommonParts/Menu.js
@@ -7,13 +7,13 @@ import {QqOutlined } from '@ant-design/icons';
 import Commons from "../Utility/Commons";
 import { Select } from 'antd';
 import { useNavigate   } from "react-router-dom";
-
+import { useCookies } from 'react-cookie'; 
 export default function Menu(props){
 
     const [users, setUsers]=useState([])
     const [userAccount, setUserAccoint]=useState("")
     const navigate  = useNavigate();
-
+    const [cookieObjectApiKey, setCookieObjectApiKey, removeCookiObjectApiKey] = useCookies(['apiKey', "id", "email","uniqueName"]);
     let searchUser=async(e)=>{
         if( e.length < 1){
             setUsers([])
@@ -29,7 +29,13 @@ export default function Menu(props){
     let moveToThisAccont=async(uniqueName)=>{
         navigate("/users/"+uniqueName)
     }
-
+    let navigateTo=()=>{
+        if(cookieObjectApiKey.apiKey){
+            navigate("/users/"+cookieObjectApiKey.uniqueName)
+        }else{
+            navigate("/")
+        }
+    }
 return (
 
    <Flex  w="100%" bg="lightblue"  zIndex="sticky" as="nav" align="center" justify={["space-between" ,"space-between" ,"space-between" ,"space-around" , "space-around"]} position={"fixed"}>
@@ -44,7 +50,7 @@ return (
         >
    
             <Box  w={["50%","50%","50%","40%","30%"]} mt={"10px"} mb={"10px"} display="flex" alignItems={"center"} justifyContent="space-between" >
-                    <QqOutlined style={{fontSize: '30px', color: "#0077FF"} } />
+                    <QqOutlined onClick={navigateTo} style={{fontSize: '30px', color: "#0077FF"} } />
                     <Select
                         showSearch
                         style={{
